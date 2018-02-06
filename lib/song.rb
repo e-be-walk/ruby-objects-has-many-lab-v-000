9c874005aa07f6db27fd4d206a43d16280b02c19
Erin Walker <erinb0317@hotmail.com>
2018-02-06 21:26:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
 class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+  end
+
+  def artist=(artist)
+    @artist = artist 
+  end
+
+  def artist_name
+    self.artist.name
+  end
 
 end
